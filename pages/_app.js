d672747dd971e89bbcd2c7405fd6a31b8b630545
jebinjeb <j.jebin1990@gmail.com>
2022-03-06 21:44:51 +0530

    removed github action

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import '../styles/tailwind.css';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from '../components/navbar';
-
-export default function Home() {
-  return (
-    <div className='min-h-screen bg-blueGray-100'>
-      <Navbar></Navbar>      
-    </div>
-  )
-}
