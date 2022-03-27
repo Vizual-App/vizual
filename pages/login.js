e6407c9020b35324fff7a24886cfe6da678bbf40
Madhu <madhu.m@nokia.com>
2022-03-27 07:06:21 +0530

    ++ added login page

---
+++ pages/login.js
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+
+export default function Home() {
+  const src = "/pattern_nextjs.png";
+
+  return (
+    <section className="relative w-full h-full py-40 min-h-screen">
+      <div className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full" style={{ "background-image": "url('https://demos.creative-tim.com/notus-nextjs/img/register_bg_2.png')" }}>
+      </div>
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4 mt-12">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className='flex-auto px-4 lg:px-10 py-10 pt-0'>
+                <div className="w-auto flex flex-initial justify-center items-center m-8">
+                  <div className='rounded-full bg-white px-3 py-2 shadow-xl'>
+                    <Image src="/logo.png" alt="logo" height="50px" width="50px" />
+                  </div>
+                </div>
+                <div className="w-auto flex flex-initial justify-center items-center">
+                  <p className='text-blueGray-800 text-xl font-bold'>Vizual</p>
+                </div>
+
+                <div className='mt-6 mb-2 text-blueGray-400 text-center font-bold'>
+                  <small>Sign in with</small>
+                </div>
+
+                <div className="btn-wrapper text-center">
+                  <button className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
+                    <div className='className="w-5 mr-2'>
+                      <Image alt="logo" height="15px" width="15px" classNamr="w-5 mr-1" src="/github.svg" />
+                    </div>
+                    Github
+                  </button>
+                  <button className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
+                    <div className='className="w-5 mr-2'>
+                      <Image alt="logo" height="13px" width="13px" src="/microsoft.svg" />
+                    </div>
+                    Microsoft
+                  </button>
+                </div>
+
+                <hr className="m-4 border-b-1 border-blueGray-300" />
+
+                <div className='text-blueGray-400 text-center font-bold'>
+                  <small>Or sign in with credentials</small>
+                </div>
+
+                <form>
+                  <div className="text-center mt-2">
+                    <button className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button">Sign In</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
